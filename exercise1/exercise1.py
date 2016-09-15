fdaaad2b37cb159ef03b7204edcdede9cd1e2829
AndreaMassari <andrea.massari.phd@gmail.com>
2016-09-15 13:33:30 -0700

    3rvers

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -5,3 +5,8 @@ def print_message():
 def print_art():
     print('(__-){')
     return
+
+def hi_fellow(name):
+    if name == 'I am an Insight Fellow':
+        print("That's awesome! How is that random forest model going?")
+    return
